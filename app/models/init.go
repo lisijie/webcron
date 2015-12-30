4288bfa3e6b17e19f294a8c5f49c084a36578391
sijie.li <lsj86@qq.com>
2015-12-30 10:24:09 +0800

    优化时间

--- app/models/init.go
+++ app/models/init.go
@@ -4,6 +4,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"net/url"
 )
 
 func Init() {
@@ -12,10 +13,15 @@ func Init() {
 	dbuser := beego.AppConfig.String("db.user")
 	dbpassword := beego.AppConfig.String("db.password")
 	dbname := beego.AppConfig.String("db.name")
+	timezone := beego.AppConfig.String("db.timezone")
 	if dbport == "" {
 		dbport = "3306"
 	}
-	dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8&loc=Asia%2FShanghai"
+	dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
+	if timezone != "" {
+		dsn = dsn + "&loc=" + url.QueryEscape(timezone)
+	}
+	beego.Debug(dsn)
 	orm.RegisterDataBase("default", "mysql", dsn)
 
 	orm.RegisterModel(new(User), new(Task), new(TaskGroup), new(TaskLog))
--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,18 @@
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(), 
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
 
 $(function () {
 	$('.subnavbar').find ('li').each (function (i) {
@@ -14,6 +29,6 @@ $(function () {
 
 function setTime(initTime,serverTime) {
 	ellapsedTime = new Date().getTime()-initTime;
-	$('#server-time').html('当前服务器时间: <strong>'+new Date(serverTime+ellapsedTime).toLocaleString()+'</strong>');
+	$('#server-time').html('当前服务器时间: <strong>'+new Date(serverTime+ellapsedTime).Format("yyyy-MM-dd hh:mm:ss")+'</strong>');
 	setTimeout('setTime('+initTime+','+serverTime+');',500);
 }
